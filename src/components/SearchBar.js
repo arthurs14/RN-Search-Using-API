c656094a4bdc7682a5e94c72b9e57d2cc58b5167
arthurs14 <arthurcs426@gmail.com>
2020-04-14 16:59:32 -0700

    styled our search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,18 +4,29 @@ import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Icon name="search" size={30} />
-      <TextInput placeholder="Search" />
+    <View style={styles.backgroundStyle}>
+      <Icon name="search" style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 15,
     backgroundColor: '#d3d3d3',
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
     marginHorizontal: 15,
   },
 });
