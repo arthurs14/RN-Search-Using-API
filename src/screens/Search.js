1d1c88126f1d31411b61518e47bdc5e5c975b17e
arthurs14 <arthurcs426@gmail.com>
2020-04-14 17:57:04 -0700

    used try-catch to catch errors when making api calls, and introduced useEffect to have an initial first search on component did mount

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,27 +6,39 @@ import yelp from '../api/yelp';
 const Search = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   // API request
-  const searchAPI = async () => {
+  const searchAPI = async searchTerm => {
+    console.log('hi there');
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'los angeles',
         },
       });
       setResults(response.data.businesses);
     } catch (err) {
-      console.log(err);
+      setErrorMessage('Something went wrong');
     }
   };
 
+  // call searchAPI when component is first rendered -> BAD CODE!
+
+  useEffect(() => {
+    searchAPI('pasta');
+  }, []);
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchAPI} />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchAPI(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
